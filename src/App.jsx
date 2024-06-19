5917f5138b5f8b275a59a83025a5ba3047b496c6
Akshai P R <akshai4610@gmail.com>
2024-06-19 19:46:03 +0530

    result log file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import Header from "./Components/Header";
 import UserInput from "./Components/userInput";
 
-function App() {
+export default function App() {
   return (
     <main>
       <Header />
-      <div id="user-input">
-          <UserInput inputValue={"Initial Investment"} value={"10000"} />
-          <UserInput inputValue={"Annual Investment"} value={"1200"} />
-          <UserInput inputValue={"Expected Return"} value={6} />
-          <UserInput inputValue={"Duration"} value={10} />
-      </div>
+      <UserInput />
     </main>
   );
 }
-
-export default App;
--- src/Components/userInput.jsx
+++ src/Components/userInput.jsx
@@ -1,10 +1,65 @@
-export default function UserInput({ inputValue, value }) {
+import { useState } from "react";
+export default function UserInput() {
+  const [UserInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function inputChange(inputValue, value) {
+    setUserInput((prev) => {
+      return {
+        ...prev,
+        [inputValue]: value,
+      };
+    });
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
-          <label>{inputValue}</label>
-          <input type="number" defaultValue={value} required/>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            onChange={(event) =>
+              inputChange('initialInvestment', event.target.value)
+            }
+            defaultValue={UserInput.initialInvestment}
+            required
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            onChange={(event) =>
+              inputChange('annualInvestment', event.target.value)
+            }
+            defaultValue={UserInput.annualInvestment}
+            required
+          />
+        </p>
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            onChange={(event) =>
+              inputChange('expectedReturn', event.target.value)
+            }
+            defaultValue={UserInput.expectedReturn}
+            required
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            onChange={(event) => inputChange('duration', event.target.value)}
+            defaultValue={UserInput.duration}
+            required
+          />
         </p>
       </div>
     </section>
