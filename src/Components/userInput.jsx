6c00434724a49376e5e697b14f3c075d9a2dbde9
Akshai P R <akshai4610@gmail.com>
2024-06-19 18:39:04 +0530

    User Inputs added

--- src/Components/userInput.jsx
+++ src/Components/userInput.jsx
@@ -1,8 +1,12 @@
 export default function UserInput({ inputValue, value }) {
   return (
-    <>
-      <label>{inputValue}</label>
-      <input type="number" defaultValue={value} />
-    </>
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>{inputValue}</label>
+          <input type="number" defaultValue={value} required/>
+        </p>
+      </div>
+    </section>
   );
 }
